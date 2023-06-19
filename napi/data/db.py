33e059bebc9592d0b3b430ab8b409fda76ff530d
Girish Mohan <mohangiri1999@gmail.com>
2023-06-19 23:38:55 +0530

    Add initial db interface
    To connect to the database for inserting info and querying, an
    interface is required. This module will provide the functionalities
    expected from an interface.
    
    sqlite is chosen as the database will primarily be used to store the
    indexed dataset information which will not place a huge load. A
    production environment such as mariadb or postgresql would be
    overkill.
    
    Bug note: Initial attempts to return session directly from
    sessionmaker as `return sessionmaker()` did not reflect changes in db
    file despite commit. Instead of returning a different session object
    each time, storing a copy of a single session and returning that in
    future calls resolved this.

---
+++ napi/data/db.py
@@ -0,0 +1,34 @@
+"""Database functionality for indexing and data access."""
+
+import os
+
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import DeclarativeBase
+
+
+class Base(DeclarativeBase):
+    pass
+
+
+class SessionManager:
+    """Represents a session with the db."""
+
+    def __init__(self, db_path=None):
+        db = get_db(db_path)
+        engine = create_engine(db, echo=True)
+        Base.metadata.create_all(engine)
+        self._sessionmaker = sessionmaker(engine)
+        self._session = None
+
+    @property
+    def session(self):
+        if self._session is None:
+            self._session = self._sessionmaker()
+        return self._session
+
+
+def get_db(db_path):
+    if not db_path:
+        db_path = os.path.join(os.path.expanduser("~"), "db_index.sqlite")
+    return f"sqlite:///{db_path}"

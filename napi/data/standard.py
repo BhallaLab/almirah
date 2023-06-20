6e7df86423beebe65f9be00f114bf7804851cce6
Girish Mohan <mohangiri1999@gmail.com>
2023-06-20 23:33:18 +0530

    Enhance readability by renaming imports and entity to tag
    We use dictionary `copy()` to create a copy of the tags of the data
    file. This makes sure that we do not change the original set of tags
    attached to the file while either trying to match the appropriate path
    pattern or creating tags for fellow files. But this conflicts with the
    `copy()` defined in utils. Importing utils circumvents this. Instead,
    we use `utils.copy()`.
    
    As per our specification design, a file can have many tags attached to
    it. To reflect this better, renaming entity to tag in standard.py.

--- napi/data/standard.py
+++ napi/data/standard.py
@@ -6,10 +6,10 @@ import warnings
 
 from string import Formatter
 
-from ..utils import get_matching_files, copy
+from .. import utils
 
 
-_PATTERN_FIND = re.compile(r"({([\w\d]*?)(?:<([^>]+)>)?(?:\|((?:\.?[\w])+))?\})")
+_TAG_PATTERN = re.compile(r"({([\w\d]*?)(?:<([^>]+)>)?(?:\|((?:\.?[\w])+))?\})")
 
 
 class Specification:
@@ -30,7 +30,7 @@ class Specification:
     Notes
     -----
     The YAML file that describes the specification can be split into two parts.
-       - entity definitions
+       - tag definitions
        - path definitions
 
     More information on these will follow.
@@ -40,13 +40,13 @@ class Specification:
         self._spec = spec
         self.name = name
 
-    def build_path(self, entities):
-        """Construct path provided a set of entities.
+    def build_path(self, tags):
+        """Construct path provided a set of tags.
 
         Parameters
         ----------
-        entities : dict
-            A dictionary with entity name, value pairs.
+        tags : dict
+            A dictionary with tag name, value pairs.
 
         Returns
         -------
@@ -57,40 +57,40 @@ class Specification:
         path_patterns = self._spec.get("path_patterns")
 
         # Remove none values
-        entities = {k: v for k, v in entities.items() if v or v == 0}
+        tags = {k: v for k, v in tags.items() if v or v == 0}
 
         # Work with extension with or without .
-        if "extension" in entities:
-            ext = entities.get("extension")
-            entities["extension"] = ext if ext.startswith(".") else "." + ext
+        if "extension" in tags:
+            ext = tags.get("extension")
+            tags["extension"] = ext if ext.startswith(".") else "." + ext
 
-        # Attempt to match pattern with entities and return first match
+        # Attempt to match pattern with tags and return first match
         for pattern in path_patterns:
             path = pattern
-            matches = _PATTERN_FIND.findall(pattern)
-            entities_defined = [e[1] for e in matches]
+            matches = _TAG_PATTERN.findall(pattern)
+            tags_defined = [t[1] for t in matches]
 
-            # Do not tamper with entities provided so that
+            # Do not tamper with tags provided so that
             # it can be used for other patterns
-            entities_copy = entities.copy()
+            tags_copy = tags.copy()
 
-            # Skip if all entities not matched
-            if set(entities_copy.keys()) - set(entities_defined):
+            # Skip if all tags not matched
+            if set(tags_copy.keys()) - set(tags_defined):
                 continue
 
-            # Validate and fill in missing entities with default
+            # Validate and fill in missing tags with default
             for subpattern, name, valid, default in matches:
                 valid_expanded = [v for v in valid.split("|")]
 
                 if (
                     valid_expanded
-                    and name in entities_copy
-                    and entities_copy[name] not in valid_expanded
+                    and name in tags_copy
+                    and tags_copy[name] not in valid_expanded
                 ):
                     continue
 
-                if name not in entities_copy and default:
-                    entities_copy[name] = default
+                if name not in tags_copy and default:
+                    tags_copy[name] = default
 
                 if valid_expanded and default and default not in valid_expanded:
                     warnings.warn(
@@ -100,13 +100,13 @@ class Specification:
                 # Simplify path
                 path = path.replace(subpattern, "{%s}" % name)
 
-            # Keep or remove optional entities
+            # Keep or remove optional tags
             optional_patterns = re.findall(r"(\[.*?\])", path)
             for op in optional_patterns:
-                optional_entity = re.findall(r"\{(.*?)\}", op)[0]
+                optional_tag = re.findall(r"\{(.*?)\}", op)[0]
                 path = (
                     path.replace(op, op[1:-1])
-                    if optional_entity in entities_copy.keys()
+                    if optional_tag in tags_copy.keys()
                     else path.replace(op, "")
                 )
 
@@ -114,11 +114,11 @@ class Specification:
             fields = [f[1] for f in Formatter().parse(path)]
 
             # Proceed only if all field data available
-            if set(fields) - set(entities_copy.keys()):
+            if set(fields) - set(tags_copy.keys()):
                 continue
 
             # Fill in the fields
-            path = path.format_map(entities_copy)
+            path = path.format_map(tags_copy)
 
             return path
 
@@ -146,64 +146,60 @@ class Specification:
             "source",
             "destination",
             "pattern",
-            "entity_rules",
+            "tag_rules",
         ]:
             if mandatory_key not in rules:
                 print(f"Missing mandatory key '{mandatory_key}' in rule")
                 return
 
         # Organize matched files as per rules
-        for file in get_matching_files(rules.get("source"), rules.get("pattern")) or []:
-            # Extract entities
-            entities = dict()
-            for rule in rules.get("entity_rules"):
-                entity_name = rule.get("name")
+        for file in (
+            utils.get_matching_files(rules.get("source"), rules.get("pattern")) or []
+        ):
+            # Extract tags
+            tags = dict()
+            for rule in rules.get("tag_rules"):
+                tag_name = rule.get("name")
 
                 if "value" in rule:
-                    entities.update({entity_name: rule.get("value")})
+                    tags.update({tag_name: rule.get("value")})
                     continue
 
                 match = re.findall(rule.get("pattern"), file)
-                entity_val = match[0] if match else None
+                tag_val = match[0] if match else None
 
-                if "prepend" in rule and entity_val:
-                    entity_val = "".join([str(rule.get("prepend")), entity_val])
+                if "prepend" in rule and tag_val:
+                    tag_val = "".join([str(rule.get("prepend")), tag_val])
 
-                if (
-                    "length" in rule
-                    and entity_val
-                    and len(entity_val) != rule.get("length")
-                ):
+                if "length" in rule and tag_val and len(tag_val) != rule.get("length"):
                     if "iffy_prepend" in rule:
-                        entity_val = "".join(
-                            [str(rule.get("iffy_prepend")), entity_val]
-                        )
-                    if len(entity_val) != rule.get("length"):
-                        entity_val = None
+                        tag_val = "".join([str(rule.get("iffy_prepend")), tag_val])
+                    if len(tag_val) != rule.get("length"):
+                        tag_val = None
 
-                if rule.get("case") in ["lower", "upper"] and entity_val:
-                    entity_val = (
-                        entity_val.lower()
+                if rule.get("case") in ["lower", "upper"] and tag_val:
+                    tag_val = (
+                        tag_val.lower()
                         if rule.get("case") == "lower"
-                        else entity_val.upper()
+                        else tag_val.upper()
                     )
 
-                if "default" in rule and not entity_val:
-                    entity_val = rule.get("default")
+                if "default" in rule and not tag_val:
+                    tag_val = rule.get("default")
 
-                if not entity_val:
+                if not tag_val:
                     warnings.warn(
-                        f"Unable to find value for entity '{entity_name}' for '{file}'"
+                        f"Unable to find value for tag '{tag_name}' for '{file}'"
                     )
 
-                entities.update({entity_name: entity_val})
+                tags.update({tag_name: tag_val})
 
             # Warning, clunky code ahead. To be made better
-            rel_path = self.build_path(entities)
+            rel_path = self.build_path(tags)
             new_path = (
                 os.path.join(rules.get("destination"), rel_path) if rel_path else None
             )
-            copy(file, new_path)
+            utils.copy(file, new_path)
 
             if not rules.get("copy_fellows", False) or not new_path:
                 continue
@@ -216,22 +212,22 @@ class Specification:
             ]
 
             for fellow in fellows:
-                # Entity changes for fellow
-                entities_copy = entities.copy()
-                entities_copy.update({"extension": os.path.splitext(fellow)[1]})
+                # Tag changes for fellow
+                tags_copy = tags.copy()
+                tags_copy.update({"extension": os.path.splitext(fellow)[1]})
                 for rule in rules.get("rename_rules", []):
                     if re.findall(rule.get("target"), fellow):
-                        entities_copy.update({"suffix": rule.get("suffix")})
+                        tags_copy.update({"suffix": rule.get("suffix")})
 
                 # Copy fellow files
-                rel_path = self.build_path(entities_copy)
+                rel_path = self.build_path(tags_copy)
                 new_path = (
                     os.path.join(rules.get("destination"), rel_path)
                     if rel_path
                     else None
                 )
 
-                copy(fellow, new_path)
+                utils.copy(fellow, new_path)
 
     def validate(self):
         """Returns True if path is valid."""

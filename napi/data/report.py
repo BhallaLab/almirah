492f57c80d57fa57661da6eeb1578ee788287115
Girish Mohan <mohangiri1999@gmail.com>
2023-06-21 00:03:28 +0530

    Add reporting to datasets or layout
    This commit adds the capability to generate high level reports on
    a dataset or layout. Simple messages regarding the layouts in the
    dataset and their subject numbers are printed.
    
    The idea is to expand this to provide lengthy wordy reports if
    needed. Later, exporting it to html or other formats is also an options.

--- napi/data/report.py
+++ napi/data/report.py
@@ -1,11 +1,44 @@
 """Module for easy report generation and data summary."""
 
+from sqlalchemy import select, distinct, func
+
+from .layout import File, Tag
+
 
 class Report:
-    """Representation of report for data."""
+    """Collection of report generating methods."""
 
-    def generate(self):
-        """Generate report.
+    @staticmethod
+    def generate(layout, level="summary"):
+        """Generate report for a layout."""
+        sess = layout.indexer.conn.session
+        n_subjects = sess.scalar(
+            select(func.count(distinct(Tag.val)))
+            .where(Tag.name == "subject")
+            .where(File.root == layout.root)
+            .join(File)
+        )
 
-        Provides ability to generate report at subject or dataset level.
-        """
+        if level == "lengthy":
+            pass
+
+        print(f"Layout ... name: {layout.name} | subjects: {n_subjects}")
+
+    @staticmethod
+    def generate_for_dataset(dataset):
+        """Generate report for a dataset."""
+
+        print(f"Dataset ... {dataset.primary.name}")
+
+        sources = set()
+        for s in dataset.sourcedata.layouts:
+            sources.add(s.name)
+        print(f"Sources ... {sources}")
+
+        ders = set()
+        for d in dataset.derivatives.layouts:
+            ders.add(d.name)
+        print(f"Pipelines ... {ders}")
+
+        for lay in dataset.layouts:
+            Report.generate(lay)

dda22318a7d6d792da1e6fb6a43d8751a30f06e0
Girish Mohan <mohangiri1999@gmail.com>
2023-07-26 22:28:52 +0530

    Add robust tag reporting
    It is not a good idea to depend on a strict tag name, previously
    subject to generate the report. It is possible that a different
    dataset project used different a naming scheme. Hence, it should be
    possible to mention the tag names that should be considered during
    report generation. This commit adds this ability. The target tags for
    counting are provided as a list.

--- napi/data/report.py
+++ napi/data/report.py
@@ -9,20 +9,26 @@ class Report:
     """Collection of report generating methods."""
 
     @staticmethod
-    def generate(layout, level="summary"):
+    def generate(layout, level="summary", tags=["subject"]):
         """Generate report for a layout."""
         sess = layout.indexer.conn.session
-        n_subjects = sess.scalar(
-            select(func.count(distinct(Tag.value)))
-            .where(Tag.name == "subject")
-            .where(File.root == layout.root)
-            .join(File)
-        )
+        tag_counts = []
+        for tag in tags:
+            count = sess.scalar(
+                select(func.count(distinct(Tag.value)))
+                .where(Tag.name == tag)
+                .where(File.root == layout.root)
+                .join(File)
+            )
+            tag_counts.append(count)
 
         if level == "lengthy":
-            pass
+            raise NotImplementedError
 
-        print(f"Layout ... name: {layout.name} | subjects: {n_subjects}")
+        # Generate report string
+        print(f"Layout ... name: {layout.name}")
+        for index, tag in enumerate(tags):
+            print(f"\t | {tag} \t: {tag_counts[index]}")
 
     @staticmethod
     def generate_for_dataset(dataset):

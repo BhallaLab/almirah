b7cbd170128375e02e3bcded9efbbd371bb0dc7a
Girish Mohan <mohangiri1999@gmail.com>
2023-08-01 01:20:23 +0530

    Updates indexing to use validate and overwrite
    Previously, a new db would be created each time the indexing was
    run. This is cumbersome. Further, it took a lot of time. This is a lot
    given that we would need to reindex as new data flows in. There are
    multiple reasons for this:
    
      1) Reduce the number of files indexed by matching for only higher
      level directories. For example, only mff instead of signal and
      metadata xml files inside.
    
      2) Add better way to update a reindexed layout and prevent
      reindexing of complete layout. Also, add only valid path instead of
      all paths.
    
      3) Possibility of online indexing during conversion or using file
      monitoring
    
    All tests will be benchmarked with the normal indexing used right now
    which takes 0.06023812294006348 seconds for the dummy test data
    set. Point 3) and 1) is not explored but can be done later. For now,
    2) is present in this commit.
    
    To update the layout with reindexing, the possible ways are:
    
      1) Use merge to retieve layout in persistent state : It is possible
      to load the layout if its exists in index. If it does, load its
      files and tags too using merge() on each layout to retireve the
      updated persistent copy if it exists. This takes
      0.0054209232330322266 seconds. But here, the object properties that
      are not part of the table are overwritten to None. For example, the
      indexer and spec in layout. This can be circumvented by only copying
      attributes relevant from one object to another. But this would not
      be clean and is more of a hack. Further, any additions to the schema
      will have to be reflected here seperately.
    
      Then, insert only if file not in list. This method should get slow
      over time as the time complexity of 'in' is O(n) for
      lists. Typically a 'in' on a list of 10 elements takes only
      3.910064697265625e-05 seconds. Very fast! We can use set, but then
      individual access using indexing is not possible.
    
      2) Use merge to retrieve file in persistent state : Alternative
      approach is to use merge() to get persistent files if they exist
      too. This has a constant time complexity and will be run before
      every file add. But this will add an overhead of around
      0.002006053924560547 seconds to every add operation.
    
      3) Use upserts: SQLalchemy recently released dialect specific upsert
      support. Using this it is possible to upsert in bulk. This is the
      fastest of the proposed solutions. But this is dialect
      specific. Further, models objects cannot be passed yet as values and
      dictionary with column attributes is needed. This should be possible
      by adding a todict() to model objects and then use them. But, this
      would be a very round about way with a lot of boiler plate code.
    
    A combination of approach 1) and 2) took 0.07434201240539551 seconds
    to complete. Around 0.01 seconds longer than benchmark. Code
    readability was not sacrificed. That's not bad :)
    
    We can try to shift to 3) at a later point once ORM support and
    language agnostic method is available. This is being worked on right
    now.
    
    Bug notes:
    
      - Checking File directly with merged layout.files will not work as
      they share a different object memory space. We can make another list
      of file paths and check, but no. Let's not go there.
    
      - merge() also adds it to session and will be commited. So, have to
      be careful.
    
      - Adding class() as default parameter to methods does not initialise
      it and leads to an error.
    
    Other changes:
    
      - Adds extensive logging.
    
      - Changes Layout.files from Set to List to enable indexing.
    
      - Uses scalars to query in Indexer over execute as scalars directly
      returns ORM models and prevents the extra processing required.
    
      - Adds utility method to File to build path if any tag is changed.

--- napi/data/layout.py
+++ napi/data/layout.py
@@ -1,8 +1,9 @@
 """Model classes to represent layouts and its components."""
 
 import os
+import logging
 
-from typing import Set
+from typing import List
 from typing import Dict
 
 from sqlalchemy import select
@@ -16,31 +17,6 @@ from sqlalchemy.orm import attribute_keyed_dict
 
 from .db import Base, SessionManager
 from .standard import Specification
-
-
-class File(Base):
-    """Generic file representation."""
-
-    __tablename__ = "files"
-    path: Mapped[str] = mapped_column("path", primary_key=True)
-    root: Mapped[str] = mapped_column("root", ForeignKey("layouts.root"))
-
-    layout: Mapped["Layout"] = relationship(back_populates="files")
-    tags: Mapped[Dict[str, "Tag"]] = relationship(
-        collection_class=attribute_keyed_dict("name"),
-        cascade="all, delete-orphan",
-    )
-
-    def __init__(self, path, root):
-        self.path = path
-        self.root = root
-
-    @property
-    def rel_path(self):
-        return os.path.relpath(self.path, self.layout.root)
-
-    def __repr__(self):
-        return f"<File path={self.rel_path}>"
 
 
 class Tag(Base):
@@ -64,37 +40,75 @@ class Tag(Base):
         return f"<Tag {self.name}: '{self.value}'>"
 
 
+class File(Base):
+    """Generic file representation."""
+
+    __tablename__ = "files"
+    path: Mapped[str] = mapped_column("path", primary_key=True)
+    root: Mapped[str] = mapped_column("root", ForeignKey("layouts.root"))
+
+    layout: Mapped["Layout"] = relationship(back_populates="files")
+    tags: Mapped[Dict[str, "Tag"]] = relationship(
+        collection_class=attribute_keyed_dict("name"),
+        cascade="all, delete-orphan",
+    )
+
+    def __init__(self, path, root):
+        self.path = path
+        self.root = root
+
+    @property
+    def rel_path(self):
+        return os.path.relpath(self.path, self.root)
+
+    def build_modified_path(self, changes):
+        "Returns the path for file given changes to tags."
+        t = {}
+        for tag_name, tag in self.tags:
+            t[tag_name] = tag.value
+        t.update(changes)
+        return self.layout.spec.build_path(t)
+
+    def __repr__(self):
+        return f"<File path={self.rel_path}>"
+
+
 class Layout(Base):
-    """Representation of the file layout in the directory."""
+    """Representation of file collection in a directory."""
 
     __tablename__ = "layouts"
     root: Mapped[str] = mapped_column(primary_key=True)
     name: Mapped[str]
 
-    files: Mapped[Set["File"]] = relationship(
+    files: Mapped[List["File"]] = relationship(
         back_populates="layout", cascade="all, delete-orphan"
     )
 
-    def __init__(self, root, name=None, spec=None, indexer=None, index_layout=True):
+    def __init__(
+        self,
+        root,
+        name=None,
+        spec=None,
+        indexer=None,
+        index=True,
+    ):
         self.root = root
         self.name = name if name else os.path.basename(root)
         self.spec = spec if spec else Specification()
-        self.indexer = indexer
-        if index_layout:
-            self.index()
+        self.indexer = indexer if indexer else Indexer()
 
-    def index(self):
-        """Run indexer over the layout."""
-        if not self.indexer:
-            self.indexer = Indexer()
-        self.indexer(self)
+        logging.info("Loading existing info if any on layout")
+        existing_info = self.indexer._merge(self)
+        self.files = existing_info.files
+        self._sa_instance_state = existing_info._sa_instance_state
+
+        if index:
+            self.indexer(self)
 
     def get_files(self, **filters):
         """Return files that match criteria."""
 
-        # TODO: warn if keys not present.
-
-        # unpack filters dict into tuple
+        # Unpack filters dict to tuple
         tag_reqs = [(name, value) for name, value in filters.items()]
 
         # Construct table of passing file paths
@@ -113,8 +127,7 @@ class Layout(Base):
             .where(File.root == self.root)
         )
 
-        res = self.indexer.get(file_filter)
-        files = [r for r, in res]
+        files = self.indexer.get(file_filter)
         return files
 
     def __repr__(self):
@@ -126,17 +139,25 @@ class Indexer:
 
     def __init__(self, session_manager=None):
         self.conn = session_manager
-
-    def __call__(self, layout):
-        self.layout = layout
-        if not self.conn:
+        if not session_manager:
             self.conn = SessionManager()
+
+    def __call__(self, layout, only_valid=True):
+        logging.info(f"Indexing layout with root {layout.root}")
+        self.layout = layout
+        self.only_valid = only_valid
         self._index_dir(self.layout.root)
 
+    def _merge(self, obj):
+        return self.conn.session.merge(obj)
+
     def _index_dir(self, dir):
+        """Iteratively index all directories in layout."""
+        logging.info(f"Indexing directory {dir}")
         SKIP_DIRS = ["sourcedata", "derivatives"]
         for content in os.listdir(dir):
             if content in SKIP_DIRS:
+                logging.info(f"Skipping content {content}")
                 continue
 
             path = os.path.join(dir, content)
@@ -144,25 +165,33 @@ class Indexer:
                 self._index_dir(path)
 
             self._index_file(path)
-        # TODO: Ignore if entry already present or upsert
         self.conn.session.commit()
 
     def _index_file(self, path):
+        """Add valid files to persistent index."""
+        logging.info(f"Indexing file at {path}")
         file = File(path, self.layout.root)
-        if file not in self.layout.files:
-            self.layout.files.add(file)
-            self._index_tags(file)
-            self.conn.session.add(file)
+        if not self.layout.spec.validate(file.rel_path) and self.only_valid:
+            logging.info("Skipping invalid file")
+            return
+        file = self._merge(file)
+        if file in self.layout.files:
+            logging.info("Skipping existing file")
+            return
+        self.layout.files.append(file)
+        self._index_tags(file)
+        self.conn.session.add(file)
 
     def _index_tags(self, file):
-        is_dir_tag = Tag(file.path, "is_dir", os.path.isdir(file.path))
-        file.tags[is_dir_tag.name] = is_dir_tag
-        tags = self.layout.spec.extract_tags(file.path)
+        """Add tags of file to persistent index."""
+        logging.info("Adding file tags")
+        tags = self.layout.spec.extract_tags(file.rel_path)
         for name, value in tags.items():
             tag = Tag(file.path, name, value)
             file.tags[tag.name] = tag
+        file.tags["is_dir"] = Tag(file.path, "is_dir", os.path.isdir(file.path))
 
     def get(self, query):
         """Run a query on db associated and return all results."""
-        res = self.conn.session.execute(query).all()
+        res = self.conn.session.scalars(query).all()
         return res

273d6b2643e98c1a182e6f93346dd1b13c137245
Girish Mohan <mohangiri1999@gmail.com>
2023-09-13 15:36:30 +0530

    Move docs from rtd theme to furo
    The furo theme seems to be more minimalistic and visually
    pleasing. So, we will be transitioning to this.
    
    The plan is for the docs to have an apprearance similar to urllib3,
    which has amazing documentation.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -14,19 +14,17 @@ release = "0.1.0"
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = ["autoapi.extension", "sphinx_rtd_theme"]
+extensions = [
+]
 
-templates_path = ["_templates"]
-exclude_patterns = []
 
-# language of source code
-autoapi_type = "python"
-
-# path to directory containing source code
-autoapi_dirs = ["../../napi"]
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = "sphinx_rtd_theme"
+html_theme = "furo"
 html_static_path = ["_static"]
+html_theme_options = {
+    "navigation_with_keys": True,
+    "sidebar_hide_name": True,
+}

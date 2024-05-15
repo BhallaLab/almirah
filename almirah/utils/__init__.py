419d4b2e737b0dd48e8fe5860d5f5f827ba3da84
Girish Mohan <mohangiri1999@gmail.com>
2024-05-15 19:52:19 +0530

    Move often used utility functions to top level for convenience

--- almirah/utils/__init__.py
+++ almirah/utils/__init__.py
@@ -1,3 +1,6 @@
+from .gen import read_yaml
+from .gen import read_multi_yaml
+
 from .lib import create_tutorial_dataset
 
-__all__ = [create_tutorial_dataset]
+__all__ = [read_yaml, read_multi_yaml, create_tutorial_dataset]
